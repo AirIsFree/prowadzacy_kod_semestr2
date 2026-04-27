16a6b47338de3621a7f2f170c5e3926dbec8c47e
AirIsFree <alaobara@gmail.com>
2026-04-27 11:06:37 +0200

    Limited player movement to console bounds

--- IntroClasses/Player.cs
+++ IntroClasses/Player.cs
@@ -14,7 +14,45 @@ public class Player
 
     public void Move(int diffX, int diffY)
     {
-        _x += diffX;
-        _y += diffY;
+        int targetX = _x + diffX;
+        int targetY = _y + diffY;
+
+        if (targetX >= 0 && targetX < Console.BufferWidth)
+        {
+            _x = targetX;
+        }
+
+        if (targetY >= 0 && targetY < Console.BufferHeight)
+        {
+            _y = targetY;
+        }
+    }
+
+    public bool TakeTurn()
+    {
+        bool isPlaying = true;
+        ConsoleKeyInfo input = Console.ReadKey(true);
+        Console.SetCursorPosition(_x, _y);
+        Console.Write(" ");
+        switch (input.Key)
+        {
+            case ConsoleKey.S:
+                Move(0, 1);
+                break;
+            case ConsoleKey.W:
+                Move(0, -1);
+                break;
+            case ConsoleKey.A:
+                Move(-1, 0);
+                break;
+            case ConsoleKey.D:
+                Move(1, 0);
+                break;
+            case ConsoleKey.Q:
+                isPlaying = false;
+                break;
+        }
+        Display();
+        return isPlaying;
     }
 }
